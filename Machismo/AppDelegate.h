d99d968f517588c776e886ab40f0e64a881e9970
Maxim Poltoratsky <mdmabook@gmail.com>
2016-03-22 04:04:51 +0200

    Initial Commit

---
+++ Machismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Machismo
+//
+//  Created by Maxim  on 3/22/16.
+//  Copyright © 2016 Maxim . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Machismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Machismo
+//
+//  Created by Maxim  on 3/22/16.
+//  Copyright © 2016 Maxim . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
